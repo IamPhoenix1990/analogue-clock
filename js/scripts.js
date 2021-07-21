edec950062fc2a924b1273e9fe196beadd4fda62
Zac <69262818+IamPhoenix1990@users.noreply.github.com>
2021-07-21 20:39:31 +0100

    Calculating minute and second hands movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
-let objClock={
+let objClock = {
     init:function(){
         this.selectingElements();
+        this.getCurrentTime();
+        this.calculatingAngles();
+        this.adjustHands();
         console.log(this);
     },
     selectingElements:function(){
         this.objHourHand = document.querySelector('.hourHand');
         this.objMinuteHand = document.querySelector('.minuteHand');
         this.objSecondHand = document.querySelector('.secondHand');
+    },
+    getCurrentTime:function(){
+        let now = new Date();
+        this.hours = now.getHours();
+        this.minutes = now.getMinutes();
+        this.seconds = now.getSeconds();
+    },
+    calculatingAngles:function(){
+        this.secondsAngle = (this.seconds/60) * 360;
+        this.minutesAngle = (((this.minutes * 60) * this.seconds) / 3600) * 360;
+
+    }, 
+    adjustHands:function(){
+        this.objSecondHand.style.transform = 'rotate('+this.secondsAngle+'deg)';
+        this.objMinuteHand.style.transform = 'rotate('+this.minutesAngle+'deg)';
     }
+
 }
 //init is initalization function, everything else will have function of its own which gets called accordingly   
 objClock.init();
